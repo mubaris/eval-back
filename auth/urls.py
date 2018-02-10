3ad8820a6ed9fb85f2ee76407ed0aa7689912b26
Mubaris NK <mubarishassannk@gmail.com>
2018-02-10 11:14:10 +0530

    Add API Schema View

--- auth/urls.py
+++ auth/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from django.views import generic
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^auth/', include('rest_auth.urls')),
-    url(r'^auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^$', generic.RedirectView.as_view(url='/api/', permanent=False)),
+    url(r'^api/$', get_schema_view()),
+    url(r'^api/auth/', include('rest_auth.urls')),
+    url(r'^api/auth/registration/', include('rest_auth.registration.urls')),
 ]
